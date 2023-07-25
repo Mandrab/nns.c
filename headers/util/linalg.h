4dd5e142672d8baaa4c094200caf2b415a66f71f
Paolo Baldini <paolobaldini01@gmail.com>
2023-07-25 13:29:30 +0200

    removed linalg functions and tests to replace them with LAPACK

--- headers/util/linalg.h
+++
@@ -1,6 +0,0 @@
-#ifndef LINALG_H
-#define LINALG_H
-
-void conjugate_gradient(float** A, float* x, float* b, int size);
-
-#endif /* LINALG_H */
--- tests/util/test_linalg.h
+++
@@ -1,6 +0,0 @@
-#ifndef TEST_LINALG_H
-#define TEST_LINALG_H
-
-Suite* linalg_suite();
-
-#endif /* TEST_LINALG_H */
