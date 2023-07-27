bb4ded66f5d653dcf713d518a7b85f255b892021
Paolo Baldini <paolobaldini01@gmail.com>
2023-07-27 14:56:34 +0200

    added comments to network.h functions and structs. updated values from float to double

--- headers/device/network.h
+++ headers/device/network.h
@@ -7,7 +7,7 @@
 #include "junction.h"
 #include "wire.h"
 
-/// Topology of the Nanowire Network.
+/// @brief Topology of the Nanowire Network.
 typedef struct
 {
     wire*       Ws;         ///< Position of the wires in the packaging.
@@ -15,17 +15,28 @@ typedef struct
     junction*   Js;         ///< Position of the identified junctions.
 } network_topology;
 
+/// @brief State of the Nanowire Network.
 typedef struct
 {
-    int     size;
-    bool**  A;
-    float** G;
-    float** Y;
-    float*  V;
+    int     size;           /// Number of wires in the network.
+    bool**  A;              /// Adjacency matrix of the Nanowire Network.
+    double** G;             /// Conductances of the equivalent electrical circuit.
+    double** Y;             /// Admittances of the equivalent electrical circuit.
+    double*  V;             /// Voltage distribution across the wires.
 } network_state;
 
-network_topology create_network(datasheet ds);
 
-network_state construe_circuit(datasheet ds, network_topology nt);
+/// @brief Create the Nanowire Network topology determining the wires and junctions distribution.
+/// 
+/// @param ds The datasheet describing the Nanowire Network.
+/// @return The topology of the created Nanowire Network.
+const network_topology create_network(const datasheet ds);
+
+/// @brief Construe the equivalent electrical circuit of the Nanowire Network.
+/// 
+/// @param ds The datasheet describing the Nanowire Network.
+/// @param nt The topology of the Nanowire Network.
+/// @return The electrical representation of the Nanowire Network.
+const network_state construe_circuit(const datasheet ds, const network_topology nt);
 
 #endif /* NETWORK_H */
