720c1fb79165afe7d57d486f692d29c4867f975a
Paolo Baldini <paolobaldini01@gmail.com>
2023-07-29 00:05:40 +0200

    created the library entrypoint including all the useful files

---
+++ headers/nns.h
@@ -0,0 +1,18 @@
+#ifndef NNS_H
+#define NNS_H
+
+#include "device/datasheet.h"
+#include "device/junction.h"
+#include "device/network.h"
+#include "device/wire.h"
+
+#include "interface/interface.h"
+
+#include "stimulator/mna.h"
+#include "stimulator/update.h"
+
+#include "util/components.h"
+#include "util/measures.h"
+#include "util/point.h"
+
+#endif /* NNS_H */
