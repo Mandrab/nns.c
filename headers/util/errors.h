4489a95ada48f93e4f5f09cf0728b0c4feedfc51
Paolo Baldini <paolobaldini01@gmail.com>
2023-07-28 23:49:57 +0200

    commented errors macros and points struct

--- headers/util/errors.h
+++ headers/util/errors.h
@@ -4,13 +4,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-/**
- * Macro to perform condition-assertion and exit
- * with the specific error-code in case of error.
- * The macro expects a boolean condition as first
- * input, a possible error code as the second and
- * a message with possible parameters later.
- */
+/// @brief Macro to perform condition-assertion and exit with the specific
+/// error-code in case of error. The macro expects a boolean condition as
+/// first input, a possible error code as the second and a message with
+/// possible parameters later.
+/// 
+/// @param EVAL The condition expected to be true.
+/// @param ERROR_CODE The error code to return in case of error.
+/// @param VARARG The formatted error message and the referenced variables.
 #define assert(EVAL, ERROR_CODE, ...)       \
     ({                                      \
         if (! (EVAL))                       \
--- headers/util/point.h
+++ headers/util/point.h
@@ -1,10 +1,11 @@
 #ifndef POINT_H
 #define POINT_H
 
+/// @brief Structure representing a point in the cartesian space.
 typedef struct
 {
-    float x;
-    float y;
+    double x;   ///< The position in the first dimension of the point.
+    double y;   ///< The position in the second dimension of the point.
 } point;
 
 #endif /* POINT_H */
