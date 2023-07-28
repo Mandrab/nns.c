566022041b092ce49cc7d32918d6db3b2f0de0b5
Paolo Baldini <paolobaldini01@gmail.com>
2023-07-28 23:49:20 +0200

    removed unused funtions and commented macros

--- headers/util/tensors.h
+++ headers/util/tensors.h
@@ -4,6 +4,12 @@
 #include <stdbool.h>
 #include <stdlib.h>
 
+/// @brief Allocate a vector with the specified number of elements of the
+/// specified type.
+/// 
+/// @param[in] TYPE The type of the array to allocate.
+/// @param[in] MACRO_LENGTH The number of elements contained in the array.
+/// @return The allocated array.
 #define vector(TYPE, MACRO_LENGTH)                                          \
     ({                                                                      \
         TYPE* VECTOR_RESULT = (TYPE*)malloc((MACRO_LENGTH) * sizeof(TYPE)); \
@@ -14,6 +20,12 @@
         VECTOR_RESULT;                                                      \
     })
 
+/// @brief Allocate a vector with the specified number of elements and with
+/// the specified type and initialize it to 0.
+/// 
+/// @param[in] TYPE The type of the array to allocate.
+/// @param[in] MACRO_LENGTH The number of elements contained in the array.
+/// @return The allocated and zero-initialized array.
 #define zeros_vector(TYPE, MACRO_LENGTH)                                    \
     ({                                                                      \
         TYPE* VECTOR_RESULT = (TYPE*)calloc((MACRO_LENGTH), sizeof(TYPE));  \
@@ -24,6 +36,15 @@
         VECTOR_RESULT;                                                      \
     })
 
+/// @brief Allocate a matrix of the specified type with the specified width
+/// and height.
+/// 
+/// @param[in] TYPE The type of the matrix to allocate.
+/// @param[in] MACRO_WIDTH The width of the matrix, i.e. size of the array of
+/// pointers.
+/// @param[in] MACRO_HEIGHT The height of the matrix, i.e. size of the arrays
+/// of values.
+/// @return The allocated matrix.
 #define matrix(TYPE, MACRO_WIDTH, MACRO_HEIGHT)                             \
     ({                                                                      \
         TYPE** MATRIX_RESULT = vector(TYPE*, (MACRO_WIDTH));                \
@@ -34,6 +55,15 @@
         MATRIX_RESULT;                                                      \
     })
 
+/// @brief Allocate a matrix of the specified type with the specified width
+/// and height and initialize it to zero.
+/// 
+/// @param[in] TYPE The type of the matrix to allocate.
+/// @param[in] MACRO_WIDTH The width of the matrix, i.e. size of the array of
+/// pointers.
+/// @param[in] MACRO_HEIGHT The height of the matrix, i.e. size of the arrays
+/// of values.
+/// @return The allocated and zero-initialized matrix.
 #define zeros_matrix(TYPE, MACRO_WIDTH, MACRO_HEIGHT)                       \
     ({                                                                      \
         TYPE** MATRIX_RESULT = zeros_vector(TYPE*, (MACRO_WIDTH));          \
@@ -44,6 +74,11 @@
         MATRIX_RESULT;                                                      \
     })
 
+/// @brief Free a matrix of the given width, including its sub-arrays.
+/// 
+/// @param[in] MATRIX The matrix to free.
+/// @param[in] MACRO_WIDTH The width of the matrix, i.e. size of the array of
+/// pointers.
 #define free_matrix(MATRIX, MACRO_WIDTH)                                    \
     ({                                                                      \
         for (int MACRO_i = 0; MACRO_i < (MACRO_WIDTH); MACRO_i++)           \
@@ -52,13 +87,5 @@
         }                                                                   \
         free(MATRIX);                                                       \
     })
-
-float vdot(float* a, float* b, int size);
-
-float* vprod(float a, float* v, int size);
-
-float* mdot(float** a, float** b, int size);
-
-float** mprod(float a, float** v, int size);
 
 #endif /* TENSORS_H */
