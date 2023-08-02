8b162227aa25d8bc79f746ad50dd8db2b82eb925
Paolo Baldini <paolobaldini01@gmail.com>
2023-08-02 14:00:00 +0200

    added io file-name patterns in config.h

--- headers/config.h
+++ headers/config.h
@@ -1,6 +1,8 @@
 #ifndef CONFIG_H
 #define CONFIG_H
 
+/* SIMULATION INFORMATION */
+
 // minimum and maximum conductance of the network
 #define Y_MIN 0.001
 #define Y_MAX 0.1
@@ -13,5 +15,11 @@
 #define ETA_D 1
 #define KP 0.0001
 #define KD 0.5
+
+/* IO INFORMATION */
+
+#define NETWORK_FILE_NAME_FORMAT "nanowire_network.%d.dat"
+#define STATE_FILE_NAME_FORMAT "network_state.%d.dat"
+#define INTERFACE_FILE_NAME_FORMAT "interface.%d.dat"
 
 #endif /* CONFIG_H */
