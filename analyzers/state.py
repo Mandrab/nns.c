19eb513c0634cd8fa75d7f7154ab89042b57a49a
Paolo Baldini <paolobaldini01@gmail.com>
2023-08-14 16:53:09 +0200

    updated dynamics analysis

--- analyzers/state.py
+++ analyzers/state.py
@@ -9,13 +9,11 @@ assert len(sys.argv) > 1, "The path to the file must be provided!"
 
 network_path, interface_path, *states_path = sys.argv[1:]
 
-ds, nt = load_network(network_path)
+ds, nt, mapping = load_network(network_path)
+nss = list(map(load_state, states_path))
+
 draw_network(ds, nt)
-draw_voltage_state(load_state(states_path[-1]))
-draw_admittance_state(load_state(states_path[-1]))
-animate_voltage_variation(
-    map(load_state, states_path)    # lazy load of the network states
-)
-animate_admittance_variation(
-    map(load_state, states_path)    # lazy load of the network states
-)
+draw_voltage_state(nt, mapping, load_state(states_path[-1]))
+draw_admittance_state(nt, mapping, load_state(states_path[-1]))
+animate_voltage_variation(nt, mapping, nss)
+animate_admittance_variation(nt, mapping, nss)
