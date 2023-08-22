fa29960054d99f293e2c21985a6379c9c8133980
Paolo Baldini <paolobaldini01@gmail.com>
2023-08-22 17:38:52 +0200

    updated visualization utils and reduced code repetition

--- analyzers/util/base.py
+++ analyzers/util/base.py
@@ -1,6 +1,6 @@
 from matplotlib.lines import Line2D
 
-def draw_wires(ax, Ws):
+def draw_wires(ax, Ws, count):
     """
     Draw the nano-wires composing the network.
     The centre of the wires is marked with a red marker. 
@@ -9,11 +9,18 @@ def draw_wires(ax, Ws):
     Ws -- the set of wires to draw.
     """
 
-    for xc, yc, xa, ya, xb, yb, _ in Ws:
-        ax.add_line(Line2D([xa, xb], [ya, yb], color=(0.42, 0.42, 0.42)))
-        ax.add_line(Line2D([xc], [yc], color='r', marker='o', ms=2))
+    for w in Ws[:count]:
+        ax.add_line(Line2D(
+            [w.start_edge.x, w.end_edge.x],
+            [w.start_edge.y, w.end_edge.y],
+            color=(0.42, 0.42, 0.42)
+        ))
+        ax.add_line(Line2D(
+            [w.centroid.x], [w.centroid.y],
+            color='r', marker='o', ms=2
+        ))
 
-def draw_junctions(ax, Js):
+def draw_junctions(ax, Js, count):
     """
     Draw the junctions as circles.
 
@@ -21,5 +28,8 @@ def draw_junctions(ax, Js):
     Js -- the set of junctions to draw.
     """
 
-    for _, _, x, y in Js:
-        ax.add_line(Line2D([x], [y], color='b', marker='o', ms=2))
+    for j in Js[:count]:
+        ax.add_line(Line2D(
+            [j.position.x], [j.position.y],
+            color='b', marker='o', ms=2
+        ))
