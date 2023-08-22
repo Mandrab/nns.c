e65b54aada6950026b7a70697aca597a770fd19c
Paolo Baldini <paolobaldini01@gmail.com>
2023-08-22 17:35:48 +0200

    updated io example to match changes

--- examples/io.c
+++ examples/io.c
@@ -25,22 +25,20 @@ int main()
 
     // interpret the NN as an electrical circuit
     const network_state ns = construe_circuit(ds, nt);
-    const int* mapping = map_components(ns.A, ds.wires_count);
 
     printf("Serializing the Nanowire Network\n");
 
     // serialize the datasheet and network topology
-    serialize_network(ds, nt, mapping, 0);
+    serialize_network(ds, nt, 0);
 
     printf("Deserializing the Nanowire Network\n");
 
     // create the data structures to de-serialize the datasheet and network topology
     datasheet loaded_ds;
     network_topology loaded_nt;
-    int* loaded_mapping;
 
     // deserialize the datasheet and network topology
-    deserialize_network(&loaded_ds, &loaded_nt, &mapping, 0);
+    deserialize_network(&loaded_ds, &loaded_nt, 0);
 
     printf("Serializing the network state\n");
 
