6fbd04469f1b2d41da9713aa84976105d97270d5
Paolo Baldini <paolobaldini01@gmail.com>
2023-09-29 10:53:41 +0200

    updated the update function to work with connected components

--- headers/stimulator/update.h
+++ headers/stimulator/update.h
@@ -2,12 +2,19 @@
 #define UPDATE_H
 
 #include "device/network.h"
+#include "util/components.h"
 
-/// @brief Update the weight of the nanowires junctions (i.e., their conductance)
-/// according to the current stimulation (i.e., voltage distribution in the network).
-/// Original model proposed by Enrique Miranda.
+/// @brief Update the weight of the nanowires junctions (i.e., their
+/// conductance) according to the current stimulation (i.e., voltage
+/// distribution in the network). The only weights updated are the ones
+/// belonging to the Connected Component. As the `cc' references the data in
+/// `ns', also this latter one is modified by the function. The original model
+/// of this function was proposed by Enrique Miranda.
 /// 
-/// @param[in, out] ns The Nanowire Network electrical state that will be updated.
-void update_conductance(network_state ns);
+/// @param[in, out] ns The Nanowire Network electrical state that contains
+/// the junctions to be updated.
+/// @param[in, out] cc The Connected Component specifying the junctions to be
+/// update.
+void update_conductance(network_state ns, connected_component cc);
 
 #endif /* UPDATE_H */
--- sources/stimulator/update.c
+++ sources/stimulator/update.c
@@ -1,36 +1,31 @@
 #include <math.h>
-#include <stdlib.h>
 
 #include "stimulator/update.h"
 #include "config.h"
 
-void update_conductance(network_state ns)
+void update_conductance(network_state ns, connected_component cc)
 {
     // iterate over all the junction indexes
-    #pragma omp parallel for collapse(2)
-    for (int i = 0; i < ns.size; i++)
+    #pragma omp parallel for
+    for (int k = 0; k < cc.js_count; k++)
     {
-        for (int j = i + 1; j < ns.size; j++)
-        {
-            if (! ns.A[i][j])
-            {
-                continue;
-            }
+        // get the index of the wires in the nanowire-network state
+        int i = cc.Ms[cc.Is[k] / cc.ws_count];
+        int j = cc.Ms[cc.Is[k] % cc.ws_count];
 
-            // calculate the delta voltage on each junction
-            double ΔV = fabs(ns.V[i] - ns.V[j]);
+        // calculate the delta voltage on each junction
+        double ΔV = fabs(ns.Vs[i] - ns.Vs[j]);
 
-            // compute the potentiation and depression coefficients
-            double kp = KP * exp(ETA_P * ΔV);
-            double kd = KD * exp(-ETA_D * ΔV);
-            double kpd = kp + kd;
+        // compute the potentiation and depression coefficients
+        double kp = KP * exp(ETA_P * ΔV);
+        double kd = KD * exp(-ETA_D * ΔV);
+        double kpd = kp + kd;
 
-            // calculate the conductance of the junction
-            double g = (ns.Y[i][j] - Y_MIN) / (Y_MAX - Y_MIN);
-            g = kp / kpd * (1 + kd / kp * g * exp(-TAU * kpd));
+        // calculate the conductance of the junction
+        double g = (cc.Ys[k] - Y_MIN) / (Y_MAX - Y_MIN);
+        g = kp / kpd * (1 + kd / kp * g * exp(-TAU * kpd));
 
-            // calculate and set circuit admittance
-            ns.Y[i][j] = ns.Y[j][i] = Y_MIN + g * (Y_MAX - Y_MIN);
-        }
+        // calculate and set circuit admittance
+        cc.Ys[k] = Y_MIN + g * (Y_MAX - Y_MIN);
     }
 }
