8c4e19e09fb9d175a1e8511fa9f76e9d5cc93728
Paolo Baldini <paolobaldini01@gmail.com>
2023-09-29 10:06:28 +0200

    added concept of connected component

---
+++ headers/device/component.h
@@ -0,0 +1,32 @@
+#ifndef COMPONENT_H
+#define COMPONENT_H
+
+/// @brief Connected component of the Nanowire Network.
+typedef struct
+{
+    int ws_count;           ///< Number of nanowires in the connected
+                            ///< component. 
+    int* Is;                ///< Linearized index of the junctions (i.e.,
+                            ///< junction(i, j) = junction(i * size + j)).
+                            ///< Ordered to specify the position of the
+                            ///< Ys(i, j) weight. The indexes i, j and the size
+                            ///< are relative to the connected component, and
+                            ///< not to the parent Nanowire Network state.
+
+    int js_count;           ///< Number of junctions in the connected
+                            ///< component.
+    double* Ys;             ///< Pointer to the sub-array of the network-state
+                            ///< containing the junctions values.
+} connected_component;
+
+/// @brief Compare two connected components according to their size and
+/// potentially the number of junctions.
+/// 
+/// @param[in] e1 Pointer to the first element to compare.
+/// @param[in] e2 Pointer to the second element to compare.
+/// @return 0 if the connected components have the same number of wires and
+/// junctions; > 0 if e1 has more wires (or in case of equality junctions) than
+/// e2; < 0 otherwise.
+int cccmp(const void* e1, const void* e2);
+
+#endif /* COMPONENT_H */
---
+++ sources/device/component.c
@@ -0,0 +1,13 @@
+#include "device/component.h"
+
+int cccmp(const void* e1, const void* e2) 
+{
+    connected_component a = *((connected_component*)e1);
+    connected_component b = *((connected_component*)e2);
+
+    if (a.ws_count == b.ws_count)
+    {
+        return a.js_count - b.js_count;
+    }
+    return a.ws_count - b.ws_count;
+}
