f549530d4aab12e9246fac700999041342ddab8b
Paolo Baldini <paolobaldini01@gmail.com>
2023-10-05 16:46:51 +0200

    updated mna stimulation according to changes

--- sources/stimulator/mna.c
+++ sources/stimulator/mna.c
@@ -6,10 +6,38 @@
 #include "stimulator/mna.h"
 #include "util/errors.h"
 
-void voltage_stimulation(network_state ns, const interface it, double* io)
+void voltage_stimulation(
+    network_state ns,
+    connected_component cc,
+    const interface it,
+    double* io
+)
 {
+    // count the total number of sources, grounds, and loads
+    int sources_count = 0;
+    int loads_count = 0;
+    int grounds_count = 0;
+
+    for (int i = 0; i < cc.ws_count; i++)
+    {
+        sources_count += it.sources_mask[cc.ws_skip + i];
+        grounds_count += it.grounds_mask[cc.ws_skip + i];
+        loads_count += it.loads_mask[cc.ws_skip + i];
+    }
+
     // calculate size of MNA matrix
-    int size = ns.size + it.sources_count - it.grounds_count;
+    int size = cc.ws_count + sources_count - grounds_count;
+
+    // count and memorize the number of grounds and sources preceding a node
+    int skips[cc.ws_count] = { };
+    int index[cc.ws_count] = { };
+
+    for (int i = 1; i < cc.ws_count; i++)
+    {
+        // count the grounds and sources according to the preceding
+        skips[i] = skips[i - 1] + it.grounds_mask[cc.ws_skip + i - 1];
+        index[i] = index[i - 1] + it.sources_mask[cc.ws_skip + i - 1];
+    }
 
     // create data structures to perform the analysis
     double A[size * size] = { };
@@ -23,72 +51,63 @@ void voltage_stimulation(network_state ns, const interface it, double* io)
     // if INFO = 0, WORK(1) returns the optimal L-WORK
     double work[size];
 
-    // count and memorize the number of grounds and sources preceding a node
-    int skips[ns.size] = { };
-    int index[ns.size] = { };
-    for (int i = 1; i < ns.size; i++)
-    {
-        skips[i] = skips[i - 1] + it.grounds_mask[i - 1];
-        index[i] = index[i - 1] + it.sources_mask[i - 1];
-    }
-
-    // fill the A matrix as required to run the MNA algorithm 
+    // fill the A matrix according to the junctions values
     #pragma omp parallel for
-    for (int i = 0; i < ns.size; i++)
+    for (int k = 0; k < cc.js_count; k++)
     {
-        // if i is a grounded node continue
-        if (it.grounds_mask[i])
-        {
-            continue;
-        }
+        int i = cc.Is[k] / cc.ws_count;
+        int j = cc.Is[k] % cc.ws_count;
 
-        // iterate over the columns of A (excluding grounds)
-        // setting the value as the negate of the conductance
-        // and summing their positive values on the diagonal
-        for (int j = 0; j < ns.size; j++)
-        {
-            // if i == j, continue to the next element (the diagonal is 0)
-            if (i == j)
-            {
-                continue;
-            }
+        bool is_i_ground = it.grounds_mask[cc.ws_skip + i];
+        bool is_j_ground = it.grounds_mask[cc.ws_skip + j];
 
+        if (! is_i_ground)
+        {
             // sum the outgoing conductances on the diagonal
             // linearization of A[i - skips[i]][i - skips[i]]
-            A[(i - skips[i]) * size + i - skips[i]] += ns.Y[i][j];
+            #pragma omp atomic
+            A[(i - skips[i]) * size + i - skips[i]] += ns.Ys[cc.js_skip + k];
+        }
 
-            // if j is a grounded node continue
-            if (it.grounds_mask[j])
-            {
-                continue;
-            }
+        if (! is_j_ground)
+        {
+            #pragma omp atomic
+            A[(j - skips[j]) * size + j - skips[j]] += ns.Ys[cc.js_skip + k];
+        }
 
-            // negate the conductance of the Y matrix
+        if (! is_i_ground && ! is_j_ground)
+        {
+            // negate the conductance of the cc.Ys matrix
             // linearization of A[i - skips[i]][j - skips[j]] and A[j - skips[j]][i - skips[i]]
             // Since LAPACK only considers the upper / lower triangular matrix,
             // setting just one of the two is enough
-            A[(i - skips[i]) * size + j - skips[j]] = - ns.Y[i][j];
+            A[(i - skips[i]) * size + j - skips[j]] = - ns.Ys[cc.js_skip + k];
         }
+    }
 
+    // fill the A matrix according to the sources and loads
+    #pragma omp parallel for
+    for (int i = 0; i < cc.ws_count; i++)
+    {
         // mark the sources with a 1 on the right and bottom most part of the matrix
-        if (it.sources_mask[i])
+        if (it.sources_mask[cc.ws_skip + i])
         {
             // linearization of:
             // A[size - it.sources_count + index[i]][i]
             // A[i][size - it.sources_count + index[i]]
             // Since LAPACK only considers the upper / lower triangular matrix,
             // setting just one of the two is enough
-            A[(i - skips[i] + 1) * size - it.sources_count + index[i]] = 1;
+            A[(i - skips[i] + 1) * size - sources_count + index[i]] = 1;
 
             // if 'i' is a source, set its voltage value in b
-            b[size - it.sources_count + index[i]] = io[i];
+            b[size - sources_count + index[i]] = io[cc.ws_skip + i];
         }
 
         // add the loads conductance to the diagonal of A
-        if (it.loads_mask[i])
+        if (it.loads_mask[cc.ws_skip + i])
         {
             // linearization of A[i - skips[i]][i - skips[i]]
-            A[(i - skips[i]) * size + i - skips[i]] += it.loads_weight[i];
+            A[(i - skips[i]) * size + i - skips[i]] += it.loads_weight[cc.ws_skip + i];
         }
     }
 
@@ -104,23 +123,26 @@ void voltage_stimulation(network_state ns, const interface it, double* io)
     dsysv_rook_(&L, &size, &b_size, A, &size, pivots, b, &size, work, &size, &info, size);
     assert(info == 0, info, "The MNA system cannot be solved!");
 
-    // set the voltages in the ns.V array and the input currents in the io array
+    // set the voltages in the ns.Vs array and the input currents in the io array
     // according to the MNA calculation
     #pragma opm parallel for
-    for (int i = 0; i < ns.size; i++)
+    for (int i = 0; i < cc.ws_count; i++)
     {
-        if (it.sources_mask[i])
+        int nsi = cc.ws_skip + i;
+
+        if (it.sources_mask[nsi])
         {
-            ns.V[i] = io[i];
-            io[i] = - b[size - it.sources_count + index[i]];
+            ns.Vs[nsi] = io[nsi];
+            io[nsi] = - b[size - sources_count + index[i]];
         }
-        else if (it.grounds_mask[i])
+        else
+        if (it.grounds_mask[nsi])
         {
-            ns.V[i] = 0;
+            ns.Vs[nsi] = 0;
         }
         else
         {
-            ns.V[i] = b[i - skips[i]];
+            ns.Vs[nsi] = b[i - skips[i]];
         }
     }
 }
