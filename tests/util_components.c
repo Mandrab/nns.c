932f7954463e4873b90bdea978eb54857647f23d
Paolo Baldini <paolobaldini01@gmail.com>
2023-10-05 09:49:59 +0200

    added and updated CC tests

--- tests/util_components.c
+++ tests/util_components.c
@@ -1,8 +1,6 @@
-#include <float.h>
-#include <stdbool.h>
-#include <stdint.h>
-#include <stdlib.h>
+#include <math.h>
 
+#include "tests.h"
 #include "util/components.h"
 #include "util/errors.h"
 #include "util/tensors.h"
@@ -11,102 +9,162 @@
 #define QUANTS_COUNT 100
 #define ACCURACY 0.001      // 0.1 % accuracy
 
-void test_fully_connected_network()
+const point p = { -1, -1 };
+
+void test_map_single_component()
 {
-    bool** A = matrix(bool, 3, 3);
-    A[0][0] = 0; A[0][1] = 1; A[0][2] = 1;
-    A[1][0] = 1; A[1][1] = 0; A[1][2] = 0;
-    A[2][0] = 1; A[2][1] = 0; A[2][2] = 0;
+    datasheet ds = { 3 };
+    junction js[2] = {
+        (junction) { 0, 1, p },
+        (junction) { 0, 2, p }
+    };
+    network_topology nt = { NULL, 2, js };
 
-    double** Y = matrix(double, 3, 3);
-    Y[0][0] = 10; Y[0][1] = 11; Y[0][2] = 12;
-    Y[1][0] = 11; Y[1][1] = 13; Y[1][2] = 14;
-    Y[2][0] = 12; Y[2][1] = 14; Y[2][2] = 15;
+    int mapping[3];
+    int result = map_components(ds, nt, mapping);
 
-    double* V = vector(double, 3);
-    V[0] = 16; V[1] = 17;  V[2] = 18;
+    assert(result == 1, -1, INT_ERROR, "result of map_component", 1, result);
 
-    const network_state ns = (network_state) { 3, A, Y, V };
-
-    int nss_count;
-
-    network_state* nss = connected_components(ns, &nss_count);
-
-    assert(nss_count == 1, -1, "Wrong number of connected components identified");
-    char* error = "The # of nodes in the CC (%d) is different from the expected 3";
-    assert(nss[0].size == 3, -1, error, nss[0]);
-
-    network_state lns = largest_component(nss, nss_count);
-
-    assert(lns.A == nss[0].A, -1, "The largest connected component does not point correctly");
+    assert(mapping[0] == 0, -1, INT_ERROR, "mapping[0]", 0, mapping[0]);
+    assert(mapping[1] == 0, -1, INT_ERROR, "mapping[1]", 0, mapping[1]);
+    assert(mapping[2] == 0, -1, INT_ERROR, "mapping[2]", 0, mapping[2]);
 }
 
-void test_disjoint_network()
+void test_map_multiple_components()
 {
-    bool** A = matrix(bool, 6, 6);
-    A[0][0] = 0; A[0][1] = 1; A[0][2] = 0; A[0][3] = 1; A[0][4] = 0; A[0][5] = 0;
-    A[1][0] = 1; A[1][1] = 0; A[1][2] = 0; A[1][3] = 0; A[1][4] = 0; A[1][5] = 0;
-    A[2][0] = 0; A[2][1] = 0; A[2][2] = 0; A[2][3] = 0; A[2][4] = 0; A[2][5] = 0;
-    A[3][0] = 1; A[3][1] = 0; A[3][2] = 0; A[3][3] = 0; A[3][4] = 0; A[3][5] = 0;
-    A[4][0] = 0; A[4][1] = 0; A[4][2] = 0; A[4][3] = 0; A[4][4] = 0; A[4][5] = 1;
-    A[5][0] = 0; A[5][1] = 0; A[5][2] = 0; A[5][3] = 0; A[5][4] = 1; A[5][5] = 0;
+    datasheet ds = { 4 };
+    junction js[1] = { (junction) { 1, 3, p } };
+    network_topology nt = { NULL, 1, js };
 
-    double** Y = matrix(double, 6, 6);
-    Y[0][0] = 21; Y[0][1] = 22; Y[0][2] = 23; Y[0][3] = 24; Y[0][4] = 25; Y[0][5] = 26;
-    Y[1][0] = 22; Y[1][1] = 27; Y[1][2] = 28; Y[1][3] = 29; Y[1][4] = 30; Y[1][5] = 31;
-    Y[2][0] = 23; Y[2][1] = 28; Y[2][2] = 32; Y[2][3] = 33; Y[2][4] = 34; Y[2][5] = 35;
-    Y[3][0] = 24; Y[3][1] = 29; Y[3][2] = 33; Y[3][3] = 36; Y[3][4] = 37; Y[3][5] = 38;
-    Y[4][0] = 25; Y[4][1] = 30; Y[4][2] = 34; Y[4][3] = 37; Y[4][4] = 39; Y[4][5] = 40;
-    Y[5][0] = 26; Y[5][1] = 31; Y[5][2] = 35; Y[5][3] = 38; Y[5][4] = 40; Y[5][5] = 41;
+    int mapping[4];
+    int result = map_components(ds, nt, mapping);
 
-    double* V = vector(double, 6);
-    V[0] = 42; V[1] = 43;  V[2] = 44; V[3] = 45; V[4] = 46;  V[5] = 47;
+    assert(result == 3, -1, INT_ERROR, "result of map_component", 3, result);
 
-    network_state ns = (network_state) { 6, A, Y, V };
+    assert(mapping[0] == 0, -1, INT_ERROR, "mapping[0]", 0, mapping[0]);
+    assert(mapping[1] == 1, -1, INT_ERROR, "mapping[1]", 1, mapping[1]);
+    assert(mapping[2] == 2, -1, INT_ERROR, "mapping[2]", 2, mapping[2]);
+    assert(mapping[3] == 1, -1, INT_ERROR, "mapping[3]", 1, mapping[3]);
+}
 
-    int nss_count;
+void test_group_nanowires()
+{
+    datasheet ds = { 5 };
+    wire ws[5] = {
+        (wire) { p, p, p, 1 },
+        (wire) { p, p, p, 2 },
+        (wire) { p, p, p, 3 },
+        (wire) { p, p, p, 4 },
+        (wire) { p, p, p, 5 }
+    };
+    junction js[2] = {
+        (junction) { 1, 3, p },
+        (junction) { 2, 4, p }
+    };
+    network_topology nt = { ws, 2, js };
+    int n2c[5] = { 1, 0, 2, 0, 2 };
 
-    network_state* nss = connected_components(ns, &nss_count);
+    group_nanowires(ds, nt, n2c, 3);
 
-    assert(nss_count == 3, -1, "Wrong number of connected components identified");
+    assert(fabs(nt.Ws[0].length - 2) < ACCURACY, -1, DOUBLE_ERROR, "nt.Ws[0].length", 2.0, nt.Ws[0].length);
+    assert(fabs(nt.Ws[1].length - 4) < ACCURACY, -1, DOUBLE_ERROR, "nt.Ws[1].length", 4.0, nt.Ws[1].length);
+    assert(fabs(nt.Ws[2].length - 1) < ACCURACY, -1, DOUBLE_ERROR, "nt.Ws[2].length", 1.0, nt.Ws[2].length);
+    assert(fabs(nt.Ws[3].length - 3) < ACCURACY, -1, DOUBLE_ERROR, "nt.Ws[3].length", 3.0, nt.Ws[3].length);
+    assert(fabs(nt.Ws[4].length - 5) < ACCURACY, -1, DOUBLE_ERROR, "nt.Ws[4].length", 5.0, nt.Ws[4].length);
 
-    char* error = "The # of nodes in the CC (%d) is different from the expected 3";
-    assert(nss[0].size == 3, -1, error, nss[0].size);
-    assert(nss[1].size == 1, -1, error, nss[1].size);
-    assert(nss[2].size == 2, -1, error, nss[2].size);
+    assert(nt.Js[0].first_wire == 0, -1, INT_ERROR, "nt.Js[0].first_wire", 0, nt.Js[0].first_wire);
+    assert(nt.Js[0].second_wire == 1, -1, INT_ERROR, "nt.Js[0].second_wire", 1, nt.Js[0].second_wire);
+    assert(nt.Js[1].first_wire == 3, -1, INT_ERROR, "nt.Js[1].first_wire", 3, nt.Js[1].first_wire);
+    assert(nt.Js[1].second_wire == 4, -1, INT_ERROR, "nt.Js[1].second_wire", 4, nt.Js[1].second_wire);
 
-    error = "Error in the matrix sub-division for the first CC";
+    assert(n2c[0] == 0, -1, INT_ERROR, "n2c[0]", 0, n2c[0]);
+    assert(n2c[1] == 0, -1, INT_ERROR, "n2c[1]", 0, n2c[1]);
+    assert(n2c[2] == 1, -1, INT_ERROR, "n2c[2]", 1, n2c[2]);
+    assert(n2c[3] == 2, -1, INT_ERROR, "n2c[3]", 2, n2c[3]);
+    assert(n2c[4] == 2, -1, INT_ERROR, "n2c[4]", 2, n2c[4]);
+}
 
-    assert(nss[0].A[0][0] == 0 && nss[0].Y[0][0] == 21, -1, error);
-    assert(nss[0].A[0][1] == 1 && nss[0].Y[0][1] == 22, -1, error);
-    assert(nss[0].A[0][2] == 1 && nss[0].Y[0][2] == 24, -1, error);
-    assert(nss[0].A[0][1] == nss[0].A[1][0] && nss[0].Y[0][1] == nss[0].Y[1][0], -1, error);
-    assert(nss[0].A[0][2] == nss[0].A[2][0] && nss[0].Y[0][2] == nss[0].Y[2][0], -1, error);
-    assert(nss[0].A[1][1] == 0 && nss[0].Y[1][1] == 27, -1, error);
-    assert(nss[0].A[1][2] == 0 && nss[0].Y[1][2] == 29, -1, error);
-    assert(nss[0].A[1][2] == nss[0].A[2][1] && nss[0].Y[1][2] == nss[0].Y[2][1], -1, error);
-    assert(nss[0].A[2][2] == 0 && nss[0].Y[2][2] == 36, -1, error);
+void test_split_single_component()
+{
+    junction js[2] = {
+        (junction) { 0, 1, (point) { -1, -1 } },
+        (junction) { 0, 2, (point) { -1, -1 } }
+    };
+    int Is[2] = { 1, 2 };
+    double Ys[2] = { 0.1, 0.2 };
+    double Vs[3] = { 0.01, 0.02, 0.03 };
 
-    error = "Error in the matrix sub-division for the second CC";
-    
-    assert(nss[1].A[0][0] == 0 && nss[1].Y[0][0] == 32, -1, error);
+    datasheet ds = { 3 };
+    network_topology nt = { NULL, 2, js };
+    network_state ns = { Is, Ys, Vs };
+    int n2c[3] = { 0, 0, 0 };
 
-    error = "Error in the matrix sub-division for the third CC";
+    connected_component cc = split_components(ds, nt, ns, n2c, 1)[0];
 
-    assert(nss[2].A[0][0] == 0 && nss[2].Y[0][0] == 39, -1, error);
-    assert(nss[2].A[0][1] == 1 && nss[2].Y[0][1] == 40, -1, error);
-    assert(nss[2].A[1][0] == 1 && nss[2].Y[1][0] == 40, -1, error);
-    assert(nss[2].A[1][1] == 0 && nss[2].Y[1][1] == 41, -1, error);
+    assert(cc.ws_count == 3, -1, INT_ERROR, "cc.ws_count", 3, cc.ws_count);
+    assert(cc.js_count == 2, -1, INT_ERROR, "cc.js_count", 2, cc.js_count);
 
-    network_state lns = largest_component(nss, nss_count);
+    assert(cc.ws_skip == 0, -1, INT_ERROR, "cc.ws_skip", 0, cc.ws_skip);
+    assert(cc.js_skip == 0, -1, INT_ERROR, "cc.js_skip", 0, cc.js_skip);
 
-    assert(lns.A == nss[0].A, -1, "The largest connected component does not point correctly");
+    assert(cc.Is[0] == 1, -1, INT_ERROR, "cc.Is[0]", 1, cc.Is[0]);
+    assert(cc.Is[1] == 2, -1, INT_ERROR, "cc.Is[1]", 2, cc.Is[1]);
+
+    assert(fabs(ns.Ys[cc.js_skip + 0] - 0.1) < ACCURACY, -1, DOUBLE_ERROR, "ns.Ys[cc.js_skip + 0]", 0.1, ns.Ys[cc.js_skip + 0]);
+    assert(fabs(ns.Ys[cc.js_skip + 1] - 0.2) < ACCURACY, -1, DOUBLE_ERROR, "ns.Ys[cc.js_skip + 1]", 0.2, ns.Ys[cc.js_skip + 1]);
+
+    assert(fabs(ns.Vs[cc.ws_skip + 0] - 0.01) < ACCURACY, -1, DOUBLE_ERROR, "ns.Vs[cc.ws_skip + 0]", 0.01, ns.Vs[cc.ws_skip + 0]);
+    assert(fabs(ns.Vs[cc.ws_skip + 1] - 0.02) < ACCURACY, -1, DOUBLE_ERROR, "ns.Vs[cc.ws_skip + 1]", 0.02, ns.Vs[cc.ws_skip + 1]);
+    assert(fabs(ns.Vs[cc.ws_skip + 2] - 0.03) < ACCURACY, -1, DOUBLE_ERROR, "ns.Vs[cc.ws_skip + 2]", 0.03, ns.Vs[cc.ws_skip + 2]);
+}
+
+void test_split_multiple_components()
+{
+    junction js[3] = {
+        (junction) { 0, 1, (point) { -1, -1 } },
+        (junction) { 0, 2, (point) { -1, -1 } },
+        (junction) { 4, 5, (point) { -1, -1 } }
+    };
+    int Is[3] = { 1, 2, 3 };
+    double Ys[3] = { 0.1, 0.2, 0.3 };
+    double Vs[6] = { 0.01, 0.02, 0.03, 0.04, 0.05, 0.06 };
+
+    datasheet ds = { 6 };
+    network_topology nt = { NULL, 3, js };
+    network_state ns = { Is, Ys, Vs };
+    int n2c[6] = { 0, 0, 1, 0, 2, 2 };
+
+    connected_component* ccs = split_components(ds, nt, ns, n2c, 3);
+
+    assert(ccs[0].ws_count == 3, -1, INT_ERROR, "ccs[0].ws_count", 3, ccs[0].ws_count);
+    assert(ccs[1].ws_count == 1, -1, INT_ERROR, "ccs[1].ws_count", 1, ccs[1].ws_count);
+    assert(ccs[2].ws_count == 2, -1, INT_ERROR, "ccs[2].ws_count", 2, ccs[2].ws_count);
+
+    assert(ccs[0].js_count == 2, -1, INT_ERROR, "ccs[0].js_count", 2, ccs[0].js_count);
+    assert(ccs[1].js_count == 0, -1, INT_ERROR, "ccs[1].js_count", 0, ccs[1].js_count);
+    assert(ccs[2].js_count == 1, -1, INT_ERROR, "ccs[2].js_count", 1, ccs[2].js_count);
+
+    assert(ccs[0].ws_skip == 0, -1, INT_ERROR, "ccs[0].ws_skip", 0, ccs[0].ws_skip);
+    assert(ccs[1].ws_skip == 3, -1, INT_ERROR, "ccs[1].ws_skip", 3, ccs[1].ws_skip);
+    assert(ccs[2].ws_skip == 4, -1, INT_ERROR, "ccs[2].ws_skip", 4, ccs[2].ws_skip);
+
+    assert(ccs[0].js_skip == 0, -1, INT_ERROR, "ccs[0].js_skip", 0, ccs[0].js_skip);
+    assert(ccs[1].js_skip == 2, -1, INT_ERROR, "ccs[1].js_skip", 2, ccs[1].js_skip);
+    assert(ccs[2].js_skip == 2, -1, INT_ERROR, "ccs[2].js_skip", 2, ccs[2].js_skip);
+
+    assert(ccs[0].Is[0] == 1, -1, INT_ERROR, "ccs[0].Is[0]", 1, ccs[0].Is[0]);
+    assert(ccs[0].Is[1] == 2, -1, INT_ERROR, "ccs[0].Is[1]", 2, ccs[0].Is[1]);
+    assert(ccs[1].Is == NULL, -1, INT_ERROR, "ccs[1].Is", NULL, ccs[1].Is);
+    assert(ccs[2].Is[0] == 1, -1, INT_ERROR, "ccs[2].Is[0]", 1, ccs[2].Is[0]);
 }
 
 int util_components()
 {
-    test_fully_connected_network();
-    test_disjoint_network();
+    test_map_single_component();
+    test_map_multiple_components();
+    test_group_nanowires();
+    test_split_single_component();
+    test_split_multiple_components();
 
     return 0;
 }
