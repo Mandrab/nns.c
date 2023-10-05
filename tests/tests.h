c7ae291e0e815e313c8fd80e7b61427f958b87eb
Paolo Baldini <paolobaldini01@gmail.com>
2023-10-05 15:44:47 +0200

    added default error strings

---
+++ tests/tests.h
@@ -0,0 +1,7 @@
+#ifndef TESTS_H
+#define TESTS_H
+
+#define INT_ERROR "Unexpected value of '%s'. Expected %d, Result %d"
+#define DOUBLE_ERROR "Unexpected value of '%s'. Expected %f, Result %f"
+
+#endif /* TESTS_H */
