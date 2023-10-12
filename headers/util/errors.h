081586c682ebd8c09ab9337ce776ae7b637275d5
Paolo Baldini <paolobaldini01@gmail.com>
2023-10-12 09:41:25 +0200

    added macro for testing without exiting the process

--- headers/util/errors.h
+++ headers/util/errors.h
@@ -7,7 +7,7 @@
 /// @brief Macro to perform condition-assertion and exit with the specific
 /// error-code in case of error. The macro expects a boolean condition as
 /// first input, a possible error code as the second and a message with
-/// possible parameters later.
+/// possible parameters later. This macro will terminate the process execution.
 /// 
 /// @param EVAL The condition expected to be true.
 /// @param ERROR_CODE The error code to return in case of error.
@@ -18,6 +18,24 @@
         {                                   \
             printf(__VA_ARGS__);            \
             exit(ERROR_CODE);               \
+        }                                   \
+    })
+
+/// @brief Macro to perform condition-assertion and return and error-code in
+/// case of an unexpected condition. The macro expects a boolean condition as
+/// first input, a possible error code as the second and a message with
+/// possible parameters later.
+/// 
+/// @param EVAL The condition expected to be true.
+/// @param ERROR_CODE The error code to return in case of error.
+/// @param VARARG The formatted error message and the referenced variables.
+/// @return The error code if the condition is false.
+#define requires(EVAL, ERROR_CODE, ...)     \
+    ({                                      \
+        if (! (EVAL))                       \
+        {                                   \
+            printf(__VA_ARGS__);            \
+            return ERROR_CODE;              \
         }                                   \
     })
 
