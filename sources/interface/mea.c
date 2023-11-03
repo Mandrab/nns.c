eeb37f10f36c9868ca4f2c7a80ba0ca6ea872b19
Paolo Baldini <paolobaldini01@gmail.com>
2023-11-03 17:52:43 +0100

    implemented mea to interface conversion function and added connection type initialization in mea data-structure

--- sources/interface/mea.c
+++ sources/interface/mea.c
@@ -3,6 +3,7 @@
 #include <string.h>
 
 #include "interface/mea.h"
+#include "util/tensors.h"
 
 MEA connect_MEA(const datasheet ds, const network_topology nt)
 {
@@ -32,6 +33,7 @@ MEA connect_MEA(const datasheet ds, const network_topology nt)
 
     // set all the electrodes as unconnected
     memset(mea.e2n, 0xff, MEA_ELECTRODES * sizeof(int));
+    memset(mea.ct,  NONE, MEA_ELECTRODES * sizeof(int));
 
     // map the electrodes index to the index of the
     // nearest nanowire (considering the centroid)
@@ -54,4 +56,44 @@ MEA connect_MEA(const datasheet ds, const network_topology nt)
     }
 
     return mea;
+}
+
+interface mea2interface(const MEA mea)
+{
+    interface it = { };
+
+    // determine the size of the arrays in the data-structure
+    for (int i = 0; i < MEA_ELECTRODES; i++)
+    {
+        it.sources_count += mea.ct[i] == SOURCE;
+        it.grounds_count += mea.ct[i] == GROUND;
+        it.loads_count   += mea.ct[i] == LOAD;
+    }
+
+    // allocate the required memory
+    it.sources_index = vector(int,    it.sources_count);
+    it.grounds_index = vector(int,    it.grounds_count);
+    it.loads_index   = vector(int,    it.loads_count);
+    it.loads_weight  = vector(double, it.loads_count);
+
+    // according to the state of each electrode, set the interface arrays
+    for (int i = 0, s = 0, g = 0, l = 0; i < MEA_ELECTRODES; i++)
+    {
+        switch (mea.ct[i])
+        {
+            case SOURCE:
+                it.sources_index[s++] = mea.e2n[i];
+                break;
+            case GROUND:
+                it.grounds_index[g++] = mea.e2n[i];
+                break;
+            case LOAD:
+                it.loads_index[l]     = mea.e2n[i];
+                it.loads_weight[l++]  = mea.ws[i];
+                break;
+            default:
+        }
+    }
+
+    return it;
 }
