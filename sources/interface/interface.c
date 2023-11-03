69f6ded684b36bc19617471356eedee9b77dbf70
Paolo Baldini <paolobaldini01@gmail.com>
2023-11-03 17:10:55 +0100

    updated names in interface.c

--- sources/interface/interface.c
+++ sources/interface/interface.c
@@ -6,29 +6,32 @@
 
 interface copy(const interface it)
 {
-    interface copy;
-    copy.mask_size = it.mask_size;
-    copy.sources_count = it.sources_count;
-    copy.sources_mask = vector(bool, it.mask_size);
-    copy.grounds_count = it.grounds_count;
-    copy.grounds_mask = vector(bool, it.mask_size);
-    copy.loads_count = it.loads_count;
-    copy.loads_mask = vector(bool, it.mask_size);
-    copy.loads_weight = vector(double, it.mask_size);
+    interface copy = (interface)
+    {
+        it.sources_count,
+        vector(int, it.sources_count),
+
+        it.grounds_count,
+        vector(int, it.grounds_count),
+
+        it.loads_count,
+        vector(int, it.loads_count),
+        vector(double, it.loads_count)
+    };
 
     // copy the memory area of the array of the original interface
-    memcpy(copy.sources_mask, it.sources_mask, it.mask_size * sizeof(bool));
-    memcpy(copy.grounds_mask, it.grounds_mask, it.mask_size * sizeof(bool));
-    memcpy(copy.loads_mask, it.loads_mask, it.mask_size * sizeof(bool));
-    memcpy(copy.loads_weight, it.loads_weight, it.mask_size * sizeof(double));
+    memcpy(copy.sources_index, it.sources_index, sizeof(int) * it.sources_count);
+    memcpy(copy.grounds_index, it.grounds_index, sizeof(int) * it.grounds_count);
+    memcpy(copy.loads_index,   it.loads_index,   sizeof(int) * it.loads_count);
+    memcpy(copy.loads_weight,  it.loads_weight,  sizeof(double) * it.loads_count);
 
     return copy;
 }
 
 void destroy_interface(interface it)
 {
-    free(it.sources_mask);
-    free(it.grounds_mask);
-    free(it.loads_mask);
+    free(it.sources_index);
+    free(it.grounds_index);
+    free(it.loads_index);
     free(it.loads_weight);
 }
