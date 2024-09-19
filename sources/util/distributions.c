c931f97190feff43cad030c182a2f9f08e40e1fc
Paolo Baldini <paolo.baldini.phd@gmail.com>
2024-09-19 11:01:13 +0200

    improved random number generation for normal distribution by relying on library functions

--- sources/util/distributions.c
+++ sources/util/distributions.c
@@ -1,12 +1,8 @@
-#include <math.h>
-#include <stdlib.h>
+#include <gsl/gsl_randist.h>
 
 #include "util/distributions.h"
 
 double normal_random(gsl_rng* rng, double µ, double σ)
 {
-    double u1 = gsl_rng_uniform(rng) / (double) RAND_MAX;
-    double u2 = gsl_rng_uniform(rng) / (double) RAND_MAX;
-    double z = sqrt(-2.0 * log(u1)) * cos(2.0 * M_PI * u2);
-    return µ + σ * z;
+    return µ + gsl_ran_gaussian(rng, σ);
 }
